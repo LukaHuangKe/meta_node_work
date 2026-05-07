d876904bd274da4bb3b0d9060cbdd323d44fe326
LukaHuang <huangke940604@gmail.com>
2026-05-07 13:30:38 +0800

    lesson6.2

--- phase2/solidity_basic/lesson6.2/calculate_libraty.sol
+++ phase2/solidity_basic/lesson6.2/calculate_libraty.sol
@@ -22,7 +22,13 @@ library MathOperations {
 
 // 使用库的合约
 contract Calculator {
+    using MathOperations for uint256;
     function calculate(uint256 x, uint256 y) public pure returns (uint256) {
         return MathOperations.add(x, y);
+    }
+
+    function testZero() public pure {
+        uint256 result = uint256(0).add(0);
+        assert(result == 0);
     }
 }
